17baf89357159d28c8789926710515345cebd503
FelixMemo <draac@ua.fm>
2020-07-21 16:26:19 +0300

    Updates

---
+++ precache-manifest.46e75527c9c3569b02284c00254c60c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf44a8c79fa4ad36422bd3cfa190d2",
+    "url": "/alivio/index.html"
+  },
+  {
+    "revision": "6b26e6ec3f047957529d",
+    "url": "/alivio/static/css/main.82a0eb54.chunk.css"
+  },
+  {
+    "revision": "bcc93c3ffca320f73b77",
+    "url": "/alivio/static/js/2.8cf82ef1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alivio/static/js/2.8cf82ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b26e6ec3f047957529d",
+    "url": "/alivio/static/js/main.df63c685.chunk.js"
+  },
+  {
+    "revision": "6e4d74d9a91335193e79",
+    "url": "/alivio/static/js/runtime-main.9eff76df.js"
+  }
+]);
